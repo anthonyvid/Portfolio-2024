322512b616bd0be537580a2bb28df8c1b5e790c7
Anthony Vidovic <anthonyvidovic@gmail.com>
2025-04-19 21:29:55 -0400

    2025 update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,6 +10,7 @@ import {
 	SiRedux,
 	SiTypescript,
 	SiVitest,
+	SiScala,
 } from "react-icons/si";
 import { AiOutlinePython } from "react-icons/ai";
 import { IoCarSportOutline } from "react-icons/io5";
@@ -51,15 +52,27 @@ const About = () => {
 					</motion.h1>
 				</div>
 				<p>
-					I’m a 5th-year software engineering student at the
-					University of Guelph, with a passion for front-end
-					development and creating applications that are both
-					beautiful and functional. I thrive in collaborative
-					environments where teams work together to achieve
-					exceptional results. When I’m not coding, you’ll find me
-					working on cars or playing soccer. Feel free to reach
-					out—I’m always up for a chat about tech, cars, or anything
-					in between!
+					I’m a software developer at Hootsuite with a passion for
+					building things—whether it’s products, tools, or ideas that
+					help people and solve real problems. I love taking projects
+					from concept to reality and thrive in fast-moving
+					environments where I can wear multiple hats and make
+					meaningful impact.
+					<br />
+					<br />
+					I studied Software Engineering at the University of Guelph,
+					where I explored everything from full-stack development to
+					launching internal tools and UI libraries during my
+					internships. That hands-on experience showed me how much I
+					enjoy not just coding, but creating things that others
+					actually use.
+					<br />
+					<br />
+					Outside of work, I’m usually finding ways to spend time
+					outdoors—whether it’s hiking, exploring new places, or just
+					staying active. I’m always happy to connect—especially if
+					it’s about startups, side projects, or anything worth
+					building.
 				</p>
 			</motion.div>
 
@@ -72,7 +85,7 @@ const About = () => {
 					className="languages"
 				>
 					<h2>Languages</h2>
-					<div>
+					<div className="technologies-icons">
 						<Tooltip text="JavaScript">
 							<FaJs />
 						</Tooltip>
@@ -87,6 +100,9 @@ const About = () => {
 						</Tooltip>
 						<Tooltip text="C">
 							<img src="c.svg" width={35} height={35} />
+						</Tooltip>
+						<Tooltip text="Scala">
+							<SiScala />
 						</Tooltip>
 					</div>
 				</motion.div>
